888a48fe569c472622b74ee889f802b92dc48198
jamessspanggg <jamespangmunwai@gmail.com>
2020-02-24 00:25:26 +0800

    Add installation of surge

--- config/travis/deploy.sh
+++ config/travis/deploy.sh
@@ -40,7 +40,7 @@ do
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
   DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
   DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
-  echo DEPLOY_DOMAIN
+  echo ${DEPLOY_DOMAIN}
   surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]
   then
