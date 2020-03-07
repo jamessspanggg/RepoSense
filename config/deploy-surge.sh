1b1beb8d2a47cfe6a0ba4272b49e697d5feee703
jamessspanggg-dsc <61899930+jamessspanggg-dsc@users.noreply.github.com>
2020-03-07 16:24:12 +0800

    Add surge deployment file

---
+++ config/deploy-surge.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Split on "/", ref: http://stackoverflow.com/a/5257398/689223
+REPO_SLUG_ARRAY=(${GITHUB_REPOSITORY//\// })
+REPO_OWNER=${REPO_SLUG_ARRAY[0]}
+REPO_NAME=${REPO_SLUG_ARRAY[1]}
+DEPLOY_PATH=./reposense-report
+DEPLOY_SUBDOMAIN="${GITHUB_PR_SHA:0:6}-sha" # get first 6 characters of commit sha
+
+# debugging purposes
+echo "Owner: ${REPO_OWNER}"
+echo "Repo: ${REPO_NAME}"
+echo "Event type: ${GITHUB_EVENT_NAME}"
+
+# replaces "/" or "." with "-"
+# sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
+# Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
+# The length is limited to 253 characters
+# https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
+DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
+echo "Deploy domain: ${DEPLOY_DOMAIN}"
+surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
+if [ "$GITHUB_EVENT_NAME" == "pull_request" ]
+then
+  curl "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/statuses/${GITHUB_PR_SHA}?access_token=${GITHUB_API_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"success\",\"context\": \"deployment/surge\", \"description\": \"Deploy domain: ${DEPLOY_DOMAIN}\", \"target_url\": \"${DEPLOY_DOMAIN}\"}"
+fi
